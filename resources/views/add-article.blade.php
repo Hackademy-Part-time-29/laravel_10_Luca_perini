0363832c5af708a4be395bcde5c2d3e186ea207d
zeros <luca.perini.illustrations@gmail.com>
2024-05-14 15:57:16 +0200

    ora possibile caricare delle foto agli articoli

--- resources/views/add-article.blade.php
+++ resources/views/add-article.blade.php
@@ -12,16 +12,35 @@
             {{session('success')}}
         </div>
         @endif
-        <form method="post" action="{{route('store-article')}}" class="article-form">
+        <form method="post" action="{{route('store-article')}}" class="article-form" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="title" required>
+                <input type="text" id="title" name="title" value={{old('title')}}>
+                @error('title')
+                <div class="fail-message">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Description:</label>
-                <textarea id="description" name="body" required></textarea>
+                <textarea id="description" name="body">{{old('body')}}</textarea>
+                @error('body')
+                <div class="fail-message">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
+            <div class="form-group">
+                <label for="fileInput">Select an Image:</label>
+                <input type="file" id="fileInput" name="cover">
+                @error('cover')
+                <div class="fail-message">
+                    {{$message}}
+                </div>
+                @enderror
+              </div>
             <button type="submit" class="btn-create">+Create Article</button>
         </form>
     </div>
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -7,7 +7,7 @@
         <div class="article-container">
             @foreach ($articles as $article)
             <div class="card">
-                <img src="https://picsum.photos/200/200" alt="Placeholder Image">
+                <img src="{{Storage::url($article->cover)}}" alt="Placeholder Image">
                 <div class="card-content">
                     <h2 class="name">{{$article->title}}</h2>
                     <h3 class="title">{{Str::limit($article->body ?? 'Corpo non presente',50)}}</h3>
