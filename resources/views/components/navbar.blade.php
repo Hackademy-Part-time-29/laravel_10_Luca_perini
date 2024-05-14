a7934d060a34be3176a4a7b6d8b8e5783d1590ba
zeros <luca.perini.illustrations@gmail.com>
2024-05-14 19:44:45 +0200

    creato form di registrazione, login e logout funzionanti

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,6 +10,26 @@
                 <li><a href={{route('add-article')}}>+Add Article</a></li>
                 <li><a href={{route('contact-us')}}>Contact Us</a></li>
             </ul>
+            @auth
+            <div class="user-section">
+                <div class="user-avatar"></div>
+                <div class="dropdown">
+                    <button class="dropbtn">Welcome back, {{auth()->user()->name}}!</button>
+                    <div class="dropdown-content">
+                        <a href="#" onclick="
+                            event.preventDefault();
+                            getElementById('form-logout').submit()">Logout</a>
+                        <form id="form-logout" action="logout" method="POST">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @else
+            <ul class="nav-links">
+                <li><a href={{route('register')}}>Register</a></li>
+            </ul> 
+            @endauth
         </div>
     </nav>
 </header>
